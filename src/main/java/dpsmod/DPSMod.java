12e58140a974a657475a289acb637f6ac92bee59
Paul Forestié <pforesti@student.42nice.fr>
2024-10-07 22:18:23 +0200

    Renamed

--- src/main/java/dpsmod/DPSMod.java
+++ src/main/java/dpsmod/DPSMod.java
@@ -1,20 +1,14 @@
 package dpsmod;
 
-import necesse.engine.gameLoop.tickManager.TickManager;
 import necesse.engine.localization.message.StaticMessage;
 import necesse.engine.modLoader.annotations.ModEntry;
-import dpsmod.patches.*;
 import necesse.engine.util.DPSTracker;
 import necesse.entity.mobs.Attacker;
 import necesse.entity.mobs.Mob;
-import necesse.entity.mobs.PlayerMob;
 import necesse.gfx.forms.Form;
 import necesse.gfx.forms.MainGameFormManager;
 import necesse.gfx.forms.components.localComponents.FormLocalLabel;
-import necesse.gfx.gameFont.FontManager;
 import necesse.gfx.gameFont.FontOptions;
-import necesse.gfx.gameFont.GameFontHandler;
-import necesse.gfx.gameFont.LoadGameFont;
 
 import java.awt.*;
 
@@ -29,7 +23,6 @@ public class DPSMod {
     public static MainGameFormManager mainGameFormManager = null;
 
     public void init() {
-        System.out.println("Hello world from DPSmod!");
         playerDPSTracker.DPS_TRACKING_TIME = 1001;
     }
 
